1cf0dcdc788ff7c475f3092831f9d20e53d2a474
shirlybattista <shirly.battista.sb@gmail.com>
2022-07-19 20:27:00 -0300

    cambios en userRouter, userController, y login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,12 +3,20 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 //Middleware para user -------------------------------------------//
+const { check } = require('express-validator');
+
+const validations = [
+    check('email').notEmpty().withMessage('Ingrasar correo electronico').bail()
+    .isEmail().withMessage('Ingresar correo electronico valido'),
+    check('password').notEmpty().withMessage('Ingresar contraseña')
+];
 
 
 const userController = require('../controllers/userController');
 
 //Definiendo las rutas ----------------------------- //
 router.get('/', userController.index);
+router.post('/', validations, userController.index);
 router.get('/perfil', userController.perfil);
 router.put('/perfil/editar', userController.editar);
 
