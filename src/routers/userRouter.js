2c6432d83800af1c49d85d655d60be6290428b1f
Josue <josephee.xp@gmail.com>
2022-07-31 22:54:54 -0600

    Cambios en admin y perfil de usuario

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -21,14 +21,12 @@ router.get('/registro', guestMiddleware, userController.registro);
 router.post('/registro', upload.single('image'), validationRegister, userController.form_registro);
 
 router.get('/perfil', userController.perfil);
-router.put('/perfil/editar', userController.editar_perfil);
+router.put('/perfil', userController.editar_perfil);
 
-<<<<<<< HEAD
 router.get('/registro', userController.registro);
-router.post('/registro', upload.single('image'), middleware.register_user, userController.form_registro);
-=======
+// router.post('/registro', upload.single('image'), middleware.register_user, userController.form_registro);
+
 router.get('/logout', userController.logout);
->>>>>>> 34d466fee927e6e6f8ddf863546db244891030b3
 
 //Exportando el router --------------------------//
 module.exports = router;
