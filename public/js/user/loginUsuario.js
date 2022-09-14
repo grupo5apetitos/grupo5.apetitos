b2e2edcaa0b0e53184a7ed5de74d0d9c01b44304
Josue <josephee.xp@gmail.com>
2022-09-13 18:17:49 -0600

    Cambios leves

--- public/js/user/loginUsuario.js
+++ public/js/user/loginUsuario.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
     // Extrayendo los id con queryselector -----------------------------------------//
-    let form  = document.querySelector('form');
+    let form = document.querySelector('form');
     let email = document.querySelector('#email');
     let btn = document.querySelector('input[type="submit"]');
     let passwd = document.querySelector('#password')
@@ -9,7 +9,7 @@ window.addEventListener('load', () => {
     email.focus();
 
     //Array donde se almacenaran los errores
-    let errors = ['',''];
+    let errors = ['', ''];
 
     //Funcion para validar correos ------------------------------------------------//
     function validarEmail(valor) {
@@ -17,47 +17,68 @@ window.addEventListener('load', () => {
         return patron.test(valor);
     }
     // Evento del boton de submit para envio del login ------------------------------------//
-        btn.addEventListener('click', function(event){
-            event.preventDefault();
-        })      
-       
-        email.addEventListener('blur', function(){
-                if (validarEmail(this.value)) {
-                    if(errors[0].length > 0){ 
-                        errors[0] = '';
-                        this.classList.add('is-valid');
-                        this.classList.remove('is-invalid');
-                        emailErrors.innerText = '';
-                    }
-                } else {
-                    if(errors[0].length == 0){ 
-                        errors[0] = 'Correo incorrecto';
-                        this.classList.remove('is-valid');
-                        this.classList.add('is-invalid');
-                        emailErrors.innerText = errors[0];
-                    }
-                
-                }
-            })
-        passwd.addEventListener('blur', function(){
-                if (passwd.value >= 3) {
-                    if(errors[1].length > 0){ 
-                        errors[1] = '';  
-                        this.classList.add('is-valid');
-                        this.classList.remove('is-invalid');
-                        passwdErrors.innerText = '';   // Reseteo a un string vacio para que no salga el mensaje
-                    }
-                } else {
-                    if(errors[1].length == 0){ 
-                        errors[1] = 'Password incorrecto';
-                        this.classList.remove('is-valid');
-                        this.classList.add('is-invalid');
-                        passwdErrors.innerText = errors[1];
-                    }
-                
-                }
-        })
-        
-    })
+    /*
+    btn.addEventListener('click', function (event) {
+
+        if (errors[0] !== '') {
+            alert('Revisa el campo de correo.');
+        }
+
+        if (errors[1] !== '') {
+            alert('Revisa el campo password.');
+        }
+
+        if (errors[0] == '' && errors[1] == '') {
+            form.submit();
+        }
+
+        event.preventDefault();
+    });
+    */
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        console.log('Estuve en el formulario');
+    });
+
+    email.addEventListener('blur', function () {
+        if (validarEmail(this.value)) {
+            if (errors[0].length > 0) {
+                errors[0] = '';
+                this.classList.add('is-valid');
+                this.classList.remove('is-invalid');
+                emailErrors.innerText = '';
+            }
+        } else {
+            if (errors[0].length == 0) {
+                errors[0] = 'Correo incorrecto';
+                this.classList.remove('is-valid');
+                this.classList.add('is-invalid');
+                emailErrors.innerText = errors[0];
+            }
+
+        }
+    });
+
+    passwd.addEventListener('blur', function () {
+        if (passwd.value.length >= 5) {
+            if (errors[1].length > 0) {
+                errors[1] = '';
+                this.classList.add('is-valid');
+                this.classList.remove('is-invalid');
+                passwdErrors.innerText = '';   // Reseteo a un string vacio para que no salga el mensaje
+            }
+        } else {
+            if (errors[1].length == 0) {
+                errors[1] = 'Password incorrecto';
+                this.classList.remove('is-valid');
+                this.classList.add('is-invalid');
+                passwdErrors.innerText = errors[1];
+            }
+
+        }
+    });
+
+})
 
 
